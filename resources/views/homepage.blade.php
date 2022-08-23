b59f4c5575b9e5dc51cad23c4c6dbe7216f56cf5
gintsauzins <gints.job@gmail.com>
2022-08-23 12:45:16 +0300

    intro container style

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/homepagestyle.css">
+</head>
+
+<body>
+    <x-navbar />
+    <div class="intro__container">
+        <div class="intro__container__text">
+            <h1>Wheels for life</h1>
+            <p>products for all types of vehicles</p>
+        </div>
+        <button class="intro__container__btn">Go to Shop</button>
+    </div>
+
+</body>
+
+
+
+
+</html>
---
+++ storage/framework/views/1c3d2933c1032ee980b563502c8f01b56eeb0572.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/homepagestyle.css">
+</head>
+
+<body>
+    <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.navbar','data' => []] + (isset($attributes) ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('navbar'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+    <div class="intro__container">
+        <div class="intro__container__text">
+            <h1>Wheels for life</h1>
+            <p>products for all types of vehicles</p>
+        </div>
+        <button class="intro__container__btn">Go to Shop</button>
+    </div>
+
+</body>
+
+
+
+
+</html><?php /**PATH /home/gintsauzins/Desktop/KGK_Project/resources/views/homepage.blade.php ENDPATH**/ ?>
