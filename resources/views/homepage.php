eed7154177a6a30b4575167506798997159e684c
gintsauzins <gints.job@gmail.com>
2022-08-23 11:42:18 +0300

    style changes

--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="homepage.scss">
+    <link rel="stylesheet" href="/styles/homepagestyle.css">
 </head>
 <header>
         <nav>
@@ -13,11 +13,11 @@
     
         <div class="header_container">
             <img src="https://images.unsplash.com/photo-1563456162378-f3b6b200289c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNhciUyMHdoZWVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" srcset="">
-                <div>
+                <div class="header__text">
                     <h1>Wheels for life</h1>
                     <p>products for all types of vehicles</p>
                 </div>
-            <button>Add to cart</button>
+            <button class="header__btn">Add to cart</button>
         </div>
 </header>
 
