a1c4f0fb4c1f6bb02cda1118f8fdbeb8323a3973
mckelvins <kalvis.reiniks@gmail.com>
2022-08-25 16:30:12 +0300

    increase key

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -1,7 +1,20 @@
-const addToCartButton = document.querySelector(".card__button");
+const addToCartButtons = document.querySelectorAll(".card__button");
 
-addToCartButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const code = addToCartButton.getAttribute("data-code");
-    console.log(code);
+addToCartButtons.forEach((addToCartButton) => {
+    addToCartButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        const code = addToCartButton.getAttribute("data-code");
+        console.log(code);
+        let quantity = 1;
+
+        let keys = Object.keys(localStorage);
+
+        if (keys.includes(code)) {
+            let currentQuantity = localStorage.getItem(code);
+            const newQuantity = parseInt(currentQuantity) + 1;
+            localStorage.setItem(code, newQuantity);
+        } else {
+            localStorage.setItem(code, quantity);
+        }
+    });
 });
