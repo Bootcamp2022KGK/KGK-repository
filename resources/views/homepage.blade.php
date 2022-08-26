c15d5fc91a4d321655c3029550431f9ad7e01776
gintsauzins <gints.job@gmail.com>
2022-08-26 14:55:49 +0300

    page titles

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KGK Blog</title>
     <link rel="stylesheet" href="/styles/homepage.css">
 </head>
 
@@ -12,14 +13,14 @@
     <x-navbar />
     <div class="intro">
         <div class="intro__top">
-            <div class="intro__text"> How ToTurn Your Car Into A Beast</div>
+            <div class="intro__text"> How To Turn Your Car Into A Beast</div>
             <img class="intro__image"
                 src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/car-fiat-500-abarth-street-2017-cars-tuning-fiat-500-hervey-dopson.jpg"
                 alt="lading rims" class="intro__image">
         </div>
         <hr class="intro__line">
         <div class="intro__blogtext">
-            <h1>Loren ipsum bla bla</h1>
+            <h1>Beast in car</h1>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus ligula eu nulla pellentesque
             placerat. Quisque tempor semper purus, a scelerisque metus laoreet facilisis. Proin urna ipsum, tincidunt
             eget bibendum in, rutrum sed lectus. Duis efficitur hendrerit condimentum. In hac habitasse platea dictumst.
@@ -29,33 +30,6 @@
             imperdiet neque malesuada, in mattis libero cursus. Curabitur rutrum est ut tincidunt porttitor. Nulla massa
             dui, commodo vitae nunc eget, venenatis tempor mauris. Duis ac ullamcorper massa, sit amet placerat dui. Nam
             nec sodales odio, non congue elit.
-            <h1>Loren ipsum bla bla</h1>
-
-            <img class="intro__image2"
-                src="https://mclear.co.uk/wp-content/uploads/2019/09/IMG_20190723_121539-1-750x348.jpg"
-                alt="lading rims" class="intro__image">
-            Nulla varius ipsum purus, id semper elit pellentesque ac. Fusce fringilla neque nec neque convallis, vitae
-            faucibus velit elementum. Phasellus sit amet orci bibendum, auctor diam ut, auctor massa. Cras iaculis velit
-            porta, laoreet odio in, pretium mi. Morbi vitae justo leo. Nam nec purus non nisl congue pretium ac eu arcu.
-            In quis aliquet urna. Duis gravida massa at sem suscipit euismod. Phasellus faucibus est quis pulvinar
-            faucibus. Aenean nec lorem ante.
-
-            Curabitur vehicula commodo enim ut consequat. Praesent auctor elit ante, id ultricies ante maximus at. Class
-            aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed in mollis dui.
-            Vestibulum id dolor vel mi laoreet ultricies. Fusce eu est et orci hendrerit gravida ut dignissim quam.
-            Quisque ac rutrum felis. Ut massa dui, tempus at turpis a, ultricies molestie nisi. Integer fermentum ante
-            vitae lorem iaculis semper. Cras auctor urna et ipsum tempus tincidunt. Pellentesque vel velit lacus. Donec
-            rutrum augue iaculis risus sagittis vestibulum. Cras turpis nisl, molestie et consequat ut, egestas feugiat
-            enim.
-            <h1>Loren ipsum bla bla</h1>
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed accumsan purus
-            felis, cursus ultricies velit convallis et. Aenean porttitor arcu vitae lacinia rhoncus. Vestibulum
-            dignissim porta leo dictum laoreet. Vestibulum sed condimentum sem. Integer eu vehicula risus. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Phasellus enim orci, scelerisque quis pharetra eu, sollicitudin
-            sit amet elit. Maecenas finibus quam sed ipsum iaculis pellentesque. Nulla tortor sem, fringilla ac
-            dignissim eu, dignissim at augue. Fusce sed imperdiet lorem. Suspendisse ac dictum enim. Donec ornare
-            hendrerit nunc, eu convallis nulla molestie et. Donec id tortor sit amet magna tincidunt posuere iaculis ac
-            elit.
 
 
 
@@ -63,7 +37,7 @@
 
 
     </div>
-    <hr class="intro__line">
+
     <x-footer />
 
 </body>
